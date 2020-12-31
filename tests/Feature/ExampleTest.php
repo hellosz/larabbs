2eaaea5e707b9cdafbbc632fe11c2f75964b8e8b
neng.tian <neng.tian@patpat.com>
2020-12-31 17:47:09 +0800

    add socialite

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Overtrue\LaravelSocialite\Socialite;
+use Overtrue\Socialite\AccessToken;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -17,5 +19,29 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testAccessToken()
+    {
+        // 创建token
+        $accessToken = new AccessToken([
+            'access_token' => '40_ELlLm6ckE1qoELaFErcchJswsKCOKLPYlAGSWqbjp98RFQn2uHOJAxI6OvsUEsytIjjTEQEcf-3uYHJQU_f4HQ',
+            'openid' => 'ousZA6CHP5h4IEfFFAMIujyL52_U',
+        ]);
+
+        // 获取用户信息
+        $driver = Socialite::driver('wechat');
+        $user = $driver->user($accessToken);
+        dd($user);
+    }
+
+    public function testCode()
+    {
+        // 根据code获取access_token
+        $code = 'CODE';
+        $driver = Socialite::driver('wechat');
+        $accessToken = $driver->getAccessToken($code);
+        $user = $driver->user($accessToken);
+        dd($user);
     }
 }
