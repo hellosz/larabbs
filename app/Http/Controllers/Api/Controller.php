0b2019d44902852d31a1e85bdac40f7fa6173140
fabulous <237241007@qq.com>
2021-01-08 00:12:57 +0800

    feat local

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller as BaseController;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * response
+     *
+     * @param $statusCode
+     * @param null $message
+     * @param int $code
+     * @return HttpException
+     */
+    protected function errorResponse($statusCode, $message=null, $code=0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
